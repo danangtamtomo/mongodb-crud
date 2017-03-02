d73a6cdb3ec4f5d071717c8bd08b8bfa1a4ab50d
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-02 08:57:24 +0700

    Fix wrong syntax

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,74 +2,77 @@ var Transaction = require('../models/transaction')
 
 var Transactions = {}
 
-Transactions.getTransactions = function (req, res, next) {
-  Transactions.find({})
-    .then(function (transaction) {
-      res.send(transaction)
-    })
-}
-
-Transactions.createTransaction = function (req, res, next) {
-  var transaction = new Transaction({
-    memberid: req.body.memberid
-    days: req.body.days,
-    out_date: req.body.out_date,
-    due_date: req.body.due_date,
-    in_date: req.body.in_date,
-    fine: req.body.fine.
-  })
-
-
-  transaction.booklist.push({_book:req.body.book_id, qty: req.body.qty})
-
-  transaction.save()
-    .then(function (transaction) {
-      res.send({
-        status: 'Ok',
-        message: 'New customer has been created',
-        customer: transaction
-      })
-    }).catch(function (err) {
-    res.send({
-      status: 'Error',
-      message: err
-    })
-  })
-}
-
-Transactions.updateTransaction = function (req, res, next) {
-  Transaction.update({
-    _id: req.params.id
-  }, {
-    $set: req.body
-  })
-    .then(function (transaction) {
-      res.send({
-        status: 'Ok',
-        message: `Transaction has been updated`,
-        updated_transaction: transaction
-      })
-    })
-}
-
-Transactions.deleteTransaction = function (req, res, next) {
-  Transaction.remove({
-    _id: req.params.id
-  })
-    .then(function () {
-      res.send({
-        status: 'Ok',
-        message: `The transaction has been deleted`
-      })
-    })
-    .catch(function (err) {
-      if (err) {
-        res.send({
-          status: 'Error',
-          message: err
+Transactions.getTransactions = function(req, res, next) {
+    Transactions.find({})
+        .then(function(transaction) {
+            res.send(transaction)
         })
-      }
+}
+
+Transactions.createTransaction = function(req, res, next) {
+    var transaction = new Transaction({
+        memberid: req.body.memberid,
+        days: req.body.days,
+        out_date: req.body.out_date,
+        due_date: req.body.due_date,
+        in_date: req.body.in_date,
+        fine: req.body.fine
     })
+
+
+    transaction.booklist.push({
+        _book: req.body.book_id,
+        qty: req.body.qty
+    })
+
+    transaction.save()
+        .then(function(transaction) {
+            res.send({
+                status: 'Ok',
+                message: 'New customer has been created',
+                customer: transaction
+            })
+        }).catch(function(err) {
+            res.send({
+                status: 'Error',
+                message: err
+            })
+        })
+}
+
+Transactions.updateTransaction = function(req, res, next) {
+    Transaction.update({
+            _id: req.params.id
+        }, {
+            $set: req.body
+        })
+        .then(function(transaction) {
+            res.send({
+                status: 'Ok',
+                message: `Transaction has been updated`,
+                updated_transaction: transaction
+            })
+        })
+}
+
+Transactions.deleteTransaction = function(req, res, next) {
+    Transaction.remove({
+            _id: req.params.id
+        })
+        .then(function() {
+            res.send({
+                status: 'Ok',
+                message: `The transaction has been deleted`
+            })
+        })
+        .catch(function(err) {
+            if (err) {
+                res.send({
+                    status: 'Error',
+                    message: err
+                })
+            }
+        })
 }
 
 module.exports = Transactions
--- models/transaction.js
+++ models/transaction.js
@@ -1,19 +1,25 @@
 var mongoose = require('mongoose')
 var transactionSchema = mongoose.Schema({
-  memberid: {
-    type: String,
-    unique: true
-  },
-  days: Number,
-  out_date: Date,
-  due_date: Date,
-  in_date: Date,
-  fine: Number,
-  booklist: [{_book: { type: Schema.Types.ObjectId, ref: 'Book' }, qty: Number}]
+    memberid: {
+        type: String,
+        unique: true
+    },
+    days: Number,
+    out_date: Date,
+    due_date: Date,
+    in_date: Date,
+    fine: Number,
+    booklist: [{
+        _book: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Book'
+        },
+        qty: Number
+    }]
 }, {
-  timestamps: true
+    timestamps: true
 })
 
-var customer = mongoose.model('Customer', customerSchema)
+var transaction = mongoose.model('Transaction', transactionSchema)
 
-module.exports = customer
+module.exports = transaction
